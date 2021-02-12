bc46b0b6cc1961e696386d17a3546ba0f76b398c
chef1yoon <chef1yoon@naver.com>
2021-02-12 12:43:27 +0900

    Node.js Express 에서 REST API 구축하기 (proxy이용)

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "홍길동",
+      birthday: "961222",
+      gender: "남자",
+      job: "대학생",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "홍길서",
+      birthday: "961262",
+      gender: "여자",
+      job: "직장인",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "홍길남",
+      birthday: "961622",
+      gender: "남자",
+      job: "백수",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
